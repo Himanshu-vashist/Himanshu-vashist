107b2c398da74eb33f7f6d1098d1fc510ae9deb1
Himanshu-vashist <143962962+Himanshu-vashist@users.noreply.github.com>
2023-10-24 23:41:45 +0530

    Add files via upload

---
+++ Project1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Project1.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+    <nav class="navbar sticky-top bg-body-tertiary">
+        <div>
+            <div class="text-nowrap bg-body-secondary border" style="width: 8rem;">
+                Securely store your Memories : Your image, our Commitment to Privacy And protection
+            </div>
+        </div>
+
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="https://imgs.search.brave.com/dUawG_wP9-G11AkXmqAjkbouD0oO2usEU30BtiCzPpI/rs:fit:500:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzA0LzUzLzQ4LzM1/LzM2MF9GXzQ1MzQ4/MzU3MV8zZjA0MmZr/M2VXQ3JtdzA1V2Zs/WnlJZTYwV3YyMFhp/TS5qcGc" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                Gallary Guard
+            </a>
+        </div>
+    </nav>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+        <!-- Cards here -->
+    </div>
+
+    <figure class="text-center">
+        <blockquote class="blockquote">
+            <p>"Life is like a camera. Just focus on what's important, capture the good times, develop from the negatives, and if things don't work out, take another shot."</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+            Unknown
+        </figcaption>
+    </figure>
+
+    <div class="class1">
+        <div class="row mb-3">
+            <label for="inputEmail" class="col-sm-7 col-form-label">Email</label>
+            <div class="col-sm-8">
+                <input type="email" class="form-control" id="inputEmail3">
+            </div>
+        </div>
+        <div class="row mb-3">
+            <label for="inputPassword" class="col-sm-8 col-form-label">Password</label>
+            <div class="col-sm-8">
+                <input type="password" class="form-control" id="inputPassword3">
+            </div>
+        </div>
+        <button type="submit" class="btn btn-primary">Sign in</button>
+        <button type="button" class="btn btn-primary">Sign up</button>
+    </div>
+
+</body>
+</html>
